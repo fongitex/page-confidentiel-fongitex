432654975f1a46859a26f3c267420880f885c6bd
fongitex <fongitex@gmail.com>
2024-11-27 19:09:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemple simple d'utilisation du JavaScript, pour afficher une alerte ou une action future si nécessaire
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("La page Politique de Confidentialité a été chargée.");
+});
